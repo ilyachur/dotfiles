e489c13c242ec96fd792a7ffab35cdf241a40e3b
Ilya Churaev <ilya.churaev@intel.com>
2019-02-25 11:12:51 +0300

    Added bash_profile

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -s ${ROOT_DIR}/.bash_profile ${HOME}/
+ln -s ${ROOT_DIR}/.bashrc ${HOME}/
+ln -s ${ROOT_DIR}/.bashrc.global ${HOME}/
+ln -s ${ROOT_DIR}/.config ${HOME}/
+ln -s ${ROOT_DIR}/.emacs.d ${HOME}/
+ln -s ${ROOT_DIR}/.gdbinit ${HOME}/
+ln -s ${ROOT_DIR}/.tmux ${HOME}/
+ln -s ${ROOT_DIR}/.tmux.conf ${HOME}/
+ln -s ${ROOT_DIR}/.vimrc ${HOME}/
+ln -s ${ROOT_DIR}/.vim ${HOME}/
+ln -s ${ROOT_DIR}/.zshrc ${HOME}/
+ln -s ${ROOT_DIR}/.zsh ${HOME}/
