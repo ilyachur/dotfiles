2666d83bb0058f3d187024ab83b803836323ae10
Ilya Churaev <ilya.churaev@intel.com>
2019-04-12 15:03:30 +0300

    Fixed install script

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ln -s ${ROOT_DIR}/.bash_profile ${HOME}/
 ln -s ${ROOT_DIR}/.bashrc ${HOME}/
 ln -s ${ROOT_DIR}/.bashrc.global ${HOME}/
-ln -s ${ROOT_DIR}/.config ${HOME}/
 ln -s ${ROOT_DIR}/.emacs.d ${HOME}/
 ln -s ${ROOT_DIR}/.gdbinit ${HOME}/
 ln -s ${ROOT_DIR}/.tmux ${HOME}/
@@ -14,5 +13,9 @@ ln -s ${ROOT_DIR}/.vimrc ${HOME}/
 ln -s ${ROOT_DIR}/.vim ${HOME}/
 ln -s ${ROOT_DIR}/.zshrc ${HOME}/
 ln -s ${ROOT_DIR}/.zsh ${HOME}/
+
+mkdir -p ${HOME}/.config
+ln -s ${ROOT_DIR}/.config/nvim ${HOME}/.config
+ln -s ${ROOT_DIR}/.config/vifm ${HOME}/.config
 
 exit 0
