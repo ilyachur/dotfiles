44c8fc8d3ed02ff181235f43e591c1b2984ac4ef
Ilya Churaev <ilya.churaev@intel.com>
2020-06-07 17:25:02 +0300

    Fix detection of TMUX version

--- .tmux/verify_tmux_version.sh
+++ .tmux/verify_tmux_version.sh
@@ -2,19 +2,19 @@
 
 verify_tmux_version () {
     tmux_home=~/.tmux
-    tmux_version="$(tmux -V | cut -c 6-)"
+    tmux_version="$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")"
 
-    # if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
+    if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/tmux_2.1_up.conf"
-    # else
-    #     tmux source-file "$tmux_home/tmux_2.1_down.conf"
-    # fi
+    else
+        tmux source-file "$tmux_home/tmux_2.1_down.conf"
+    fi
     # Select theme
-    # if [[ $(echo "$tmux_version > 2.8" | bc) -eq 1 ]] ; then
+    if [[ $(echo "$tmux_version > 2.8" | bc) -eq 1 ]] ; then
         tmux source-file "$tmux_home/colors/tmux-theme_2.9_up.conf"
-    # else
-    #     tmux source-file "$tmux_home/colors/tmux-theme.conf"
-    # fi
+    else
+        tmux source-file "$tmux_home/colors/tmux-theme.conf"
+    fi
 }
 
 verify_tmux_version
